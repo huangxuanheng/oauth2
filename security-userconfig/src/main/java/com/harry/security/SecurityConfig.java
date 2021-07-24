0358ba0d545dc272101914db899e6a9ca193dd15
huangxuanheng <huangxh@authine.com>
2021-07-24 22:30:03 +0800

    spring security user config

---
+++ security-userconfig/src/main/java/com/harry/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.harry.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author harry
+ * @email: huangxuanheng@163.com
+ * @des:
+ * @DATE: 2021/7/24 0024
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("harry")
+                .password("123456").roles("admin");
+    }
+}
